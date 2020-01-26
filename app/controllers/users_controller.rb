940ff1204a0981afd678b10445f188caa3325bea
vinothkumar <vinothkumar.suppaiya@ltts.com>
2020-01-25 20:56:35 -1200

    completion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,89 @@
 class UsersController < ApplicationController
-	def new 
-		@user=User.new
-	end
-    
-	def create
-		@user = User.new(user_params)
-		if @user.save
-		flash[:success]="welcome to the alpha blog#{@user.username}"
-		redirect_to articles_path
-	    else
-		render 'new'
-		end
+
+	before_action :set_user, only: [:edit, :update, :show]
+	
+	before_action :require_same_user, only: [:edit, :update]
+	
+	def index
+	
+	@users = User.paginate(page: params[:page], per_page: 5)
+	
 	end
 	
-	def edit 
-		@user =User.find(params[:id])
+	def new
+	
+	@user = User.new
+	
 	end
+	
+	def create
+	
+	@user = User.new(user_params)
+	
+	if @user.save
+	
+	flash[:success] = "Welcome to the alpha blog #{@user.username}"
+	
+	redirect_to articles_path
+	
+	else
+	
+	render 'new'
+	
+	end
+	
+	end
+	
+	def edit
+	
+	end
+	
 	def update
-		@user=User.find(params[:id])
-	    if @user.update(user_params)
-		flash[:success]="your account was update successfully"	
-		redirect_to articles_path
-		else
-		render 'edit'
-		end
+	
+	if @user.update(user_params)
+	
+	flash[:success] = "Your account was updated successfully"
+	
+	redirect_to articles_path
+	
+	else
+	
+	render 'edit'
+	
 	end
-	def show 
-		@user=User.find(params[:id])
+	
 	end
+	
+	def show
+	
+	@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+	
+	end
+	
 	private
+	
 	def user_params
-		params.require(:user).permit(:username,:email,:password)
+	
+	params.require(:user).permit(:username, :email, :password)
+	
 	end
-
-
-end
+	
+	def set_user
+	
+	@user = User.find(params[:id])
+	
+	end
+	
+	def require_same_user
+	
+	if current_user != @user
+	
+	flash[:danger] = "You can only edit your own account"
+	
+	redirect_to root_path
+	
+	end
+	
+	end
+	
+	end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ get 'about',to:'welcome#about'
 
 resources :articles 
 get 'signup', to: 'users#new'
+get 'login', to: 'sessions#new'
 
+post 'login', to: 'sessions#create'
+
+delete 'logout', to: 'sessions#destroy'
 resources :users, except: [:new]
 end
